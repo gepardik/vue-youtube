cc9a174eaa0e847007fd7a414714d21eea8ce065
gepardik <57765112+gepardik@users.noreply.github.com>
2021-09-02 14:22:58 +0300

    Lessons 3 and 4

--- src/layouts/EmptyLayout.vue
+++ src/layouts/EmptyLayout.vue
@@ -1,13 +1,5 @@
 <template>
-$END$
+  <div class="grey darken-1 empty-layout">
+    <router-view />
+  </div>
 </template>
-
-<script>
-export default {
-name: "EmptyLayout"
-}
-</script>
-
-<style scoped>
-
-</style>
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -1,13 +1,34 @@
+
+
 <template>
-$END$
+  <div class="app-main-layout">
+    <Navbar @hamburgerClick="isOpen = !isOpen" />
+    <Sidebar  v-model="isOpen" />
+    <main class="app-content" :class="{full: !isOpen}">
+      <div class="app-page">
+        <router-view />
+      </div>
+    </main>
+
+    <div class="fixed-action-btn">
+      <router-link class="btn-floating btn-large blue" to="/record">
+        <i class="large material-icons">add</i>
+      </router-link>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {
-name: "MainLayout"
-}
+import Navbar from '../components/app/Navbar'
+import Sidebar from '../components/app/Sidebar'
+
+  export default {
+    data: () => ({
+      isOpen: true
+    }),
+    name: 'main-layout',
+    components: {
+      Navbar, Sidebar
+    }
+  }
 </script>
-
-<style scoped>
-
-</style>
